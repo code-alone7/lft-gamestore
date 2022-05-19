41ccdc1719763d504536b871c9ddb59a0707ab8e
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-19 16:47:23 +0500

    create social component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,10 +6,6 @@
     </div>
   </div>
   <div class="footer__social-block">
-    <ul class="social-block__list bcg-social">
-      <li class="social-block__list__item"><a href="#" class="social-block__list__item__link"><i class="fa fa-facebook"></i></a></li>
-      <li class="social-block__list__item"><a href="#" class="social-block__list__item__link"><i class="fa fa-twitter"></i></a></li>
-      <li class="social-block__list__item"><a href="#" class="social-block__list__item__link"><i class="fa fa-instagram"></i></a></li>
-    </ul>
+    <x-social/>
   </div>
 </footer>
