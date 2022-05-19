89b3b24fb77d61718032766dd42584cc015aef6e
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-19 16:02:12 +0500

    create sidebar.news component

---
+++ resources/views/components/sidebar/news.blade.php
@@ -0,0 +1,14 @@
+<div class="sidebar-news">
+  <div class="sidebar-news__item">
+    <div class="sidebar-news__item__preview-news"><img src="img/cover/game-2.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
+    <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
+  </div>
+  <div class="sidebar-news__item">
+    <div class="sidebar-news__item__preview-news"><img src="img/cover/game-1.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
+    <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
+  </div>
+  <div class="sidebar-news__item">
+    <div class="sidebar-news__item__preview-news"><img src="img/cover/game-4.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
+    <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
+  </div>
+</div>
