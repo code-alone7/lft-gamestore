325432d61ab0975b5460616e588777886b720d96
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-19 15:59:04 +0500

    create sidebar.categories component

--- resources/views/components/sidebar/index.blade.php
+++ resources/views/components/sidebar/index.blade.php
@@ -2,13 +2,7 @@
   <div class="sidebar-item">
     <div class="sidebar-item__title">Категории</div>
     <div class="sidebar-item__content">
-      <ul class="sidebar-category">
-        <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Action</a></li>
-        <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">RPG</a></li>
-        <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Квесты</a></li>
-        <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Онлайн-игры</a></li>
-        <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Стратегии</a></li>
-      </ul>
+      <x-sidebar.categories/>
     </div>
   </div>
   <div class="sidebar-item">
