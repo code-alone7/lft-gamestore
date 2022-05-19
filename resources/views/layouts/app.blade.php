97dcd2ff2a93e32aecce883871e26088e52a7a2d
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-19 16:33:25 +0500

    create content component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,42 +21,9 @@
       </x-header>
       <div class="middle">
         <x-sidebar/>
-        <div class="main-content">
-          <div class="content-top">
-            <div class="content-top__text">Купить игры неборого без регистрации смс с торента, получить компкт диск, скачать Steam игры после оплаты</div>
-            <div class="slider"><img src="img/slider.png" alt="Image" class="image-main"></div>
-          </div>
-          <div class="content-middle">
-            <div class="content-head__container">
-              <div class="content-head__title-wrap">
-                <div class="content-head__title-wrap__title bcg-title">{{ $header }}</div>
-              </div>
-              <div class="content-head__search-block">
-                <div class="search-container">
-                  <form class="search-container__form">
-                    <input type="text" class="search-container__form__input">
-                    <button class="search-container__form__btn">search</button>
-                  </form>
-                </div>
-              </div>
-            </div>
-            <div class="content-main__container">
-              {{ $slot }}
-            </div>
-            <div class="content-footer__container">
-              <ul class="page-nav">
-                <li class="page-nav__item"><a href="#" class="page-nav__item__link"><i class="fa fa-angle-double-left"></i></a></li>
-                <li class="page-nav__item"><a href="#" class="page-nav__item__link">1</a></li>
-                <li class="page-nav__item"><a href="#" class="page-nav__item__link">2</a></li>
-                <li class="page-nav__item"><a href="#" class="page-nav__item__link">3</a></li>
-                <li class="page-nav__item"><a href="#" class="page-nav__item__link">4</a></li>
-                <li class="page-nav__item"><a href="#" class="page-nav__item__link">5</a></li>
-                <li class="page-nav__item"><a href="#" class="page-nav__item__link"><i class="fa fa-angle-double-right"></i></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="content-bottom"></div>
-        </div>
+        <x-content :header="$header">
+          {{ $slot }}
+        </x-content>
       </div>
       <footer class="footer">
         <div class="footer__footer-content">
