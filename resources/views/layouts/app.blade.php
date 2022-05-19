e5b0036ad5a6e0bf1a828358d07da17551497d67
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-19 15:57:05 +0500

    create sidebar component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,39 +20,7 @@
         <div class="authorization-block"><a href="#" class="authorization-block__link">Регистрация</a><a href="#" class="authorization-block__link">Войти</a></div>
       </x-header>
       <div class="middle">
-        <div class="sidebar">
-          <div class="sidebar-item">
-            <div class="sidebar-item__title">Категории</div>
-            <div class="sidebar-item__content">
-              <ul class="sidebar-category">
-                <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Action</a></li>
-                <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">RPG</a></li>
-                <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Квесты</a></li>
-                <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Онлайн-игры</a></li>
-                <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Стратегии</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="sidebar-item">
-            <div class="sidebar-item__title">Последние новости</div>
-            <div class="sidebar-item__content">
-              <div class="sidebar-news">
-                <div class="sidebar-news__item">
-                  <div class="sidebar-news__item__preview-news"><img src="img/cover/game-2.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
-                  <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
-                </div>
-                <div class="sidebar-news__item">
-                  <div class="sidebar-news__item__preview-news"><img src="img/cover/game-1.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
-                  <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
-                </div>
-                <div class="sidebar-news__item">
-                  <div class="sidebar-news__item__preview-news"><img src="img/cover/game-4.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
-                  <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <x-sidebar/>
         <div class="main-content">
           <div class="content-top">
             <div class="content-top__text">Купить игры неборого без регистрации смс с торента, получить компкт диск, скачать Steam игры после оплаты</div>
