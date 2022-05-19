e71feaaf4a619c93d67f152f283ca201aa7022ea
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-19 16:42:59 +0500

    create search component

--- resources/views/components/content/index.blade.php
+++ resources/views/components/content/index.blade.php
@@ -8,12 +8,7 @@
         <div class="content-head__title-wrap__title bcg-title">{{ $header }}</div>
       </div>
       <div class="content-head__search-block">
-        <div class="search-container">
-          <form class="search-container__form">
-            <input type="text" class="search-container__form__input">
-            <button class="search-container__form__btn">search</button>
-          </form>
-        </div>
+        <x-search/>
       </div>
     </div>
     <div class="content-main__container">
