950db98c1bb1a52a1287a342b10edb0bbfb9b36c
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-19 15:27:42 +0500

    create home page

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -7,11 +7,32 @@ use Illuminate\View\Component;
 class AppLayout extends Component
 {
     /**
+     * Title tag content.
+     *
+     * @var string
+     */
+    public string $title = '';
+
+    /**
+     * Header of the page.
+     *
+     * @var string
+     */
+    public string $header = '';
+
+
+    public function __construct(string $title, string $header)
+    {
+        $this->title = $title;
+        $this->header = $header;
+    }
+
+    /**
      * Get the view / contents that represents the component.
      *
      * @return \Illuminate\View\View
      */
-    public function render()
+    public function render(): \Illuminate\View\View
     {
         return view('layouts.app');
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
           <div class="content-middle">
             <div class="content-head__container">
               <div class="content-head__title-wrap">
-                <div class="content-head__title-wrap__title bcg-title">Игры в разделе action</div>
+                <div class="content-head__title-wrap__title bcg-title">{{ $header }}</div>
               </div>
               <div class="content-head__search-block">
                 <div class="search-container">
@@ -92,38 +92,7 @@
               </div>
             </div>
             <div class="content-main__container">
-              <div class="products-category__list">
-                <div class="products-category__list__item">
-                  <div class="products-category__list__item__title-product"><a href="#">Batman - Telltale Game Series</a></div>
-                  <div class="products-category__list__item__thumbnail"><a href="#" class="products-category__list__item__thumbnail__link"><img src="img/cover/game-6.jpg" alt="Preview-image"></a></div>
-                  <div class="products-category__list__item__description"><span class="products-price">400 руб.</span><a href="#" class="btn btn-blue">Купить</a></div>
-                </div>
-                <div class="products-category__list__item">
-                  <div class="products-category__list__item__title-product"><a href="#">Deus Ex: Mankind Divided</a></div>
-                  <div class="products-category__list__item__thumbnail"><a href="#" class="products-category__list__item__thumbnail__link"><img src="img/cover/game-3.jpg" alt="Preview-image"></a></div>
-                  <div class="products-category__list__item__description"><span class="products-price">400 руб.</span><a href="#" class="btn btn-blue">Купить</a></div>
-                </div>
-                <div class="products-category__list__item">
-                  <div class="products-category__list__item__title-product"><a href="#">The Witcher 3: Wild Hunt</a></div>
-                  <div class="products-category__list__item__thumbnail"><a href="#" class="products-category__list__item__thumbnail__link"><img src="img/cover/game-1.jpg" alt="Preview-image"></a></div>
-                  <div class="products-category__list__item__description"><span class="products-price">400 руб.</span><a href="#" class="btn btn-blue">Купить</a></div>
-                </div>
-                <div class="products-category__list__item">
-                  <div class="products-category__list__item__title-product"><a href="#">Rocket League</a></div>
-                  <div class="products-category__list__item__thumbnail"><a href="#" class="products-category__list__item__thumbnail__link"><img src="img/cover/game-7.jpg" alt="Preview-image"></a></div>
-                  <div class="products-category__list__item__description"><span class="products-price">400 руб.</span><a href="#" class="btn btn-blue">Купить</a></div>
-                </div>
-                <div class="products-category__list__item">
-                  <div class="products-category__list__item__title-product"><a href="#">Dishonored 2</a></div>
-                  <div class="products-category__list__item__thumbnail"><a href="#" class="products-category__list__item__thumbnail__link"><img src="img/cover/game-8.jpg" alt="Preview-image"></a></div>
-                  <div class="products-category__list__item__description"><span class="products-price">400 руб.</span><a href="#" class="btn btn-blue">Купить</a></div>
-                </div>
-                <div class="products-category__list__item">
-                  <div class="products-category__list__item__title-product"><a href="#">This War of Mine</a></div>
-                  <div class="products-category__list__item__thumbnail"><a href="#" class="products-category__list__item__thumbnail__link"><img src="img/cover/game-9.jpg" alt="Preview-image"></a></div>
-                  <div class="products-category__list__item__description"><span class="products-price">400 руб.</span><a href="#" class="btn btn-blue">Купить</a></div>
-                </div>
-              </div>
+              {{ $slot }}
             </div>
             <div class="content-footer__container">
               <ul class="page-nav">
