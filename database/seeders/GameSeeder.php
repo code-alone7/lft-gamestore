9747b9c5cdb9cf0225c3826b0e86deceb61d3a12
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-20 15:39:59 +0500

    increase amount of generated data in GameSeeder

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -16,7 +16,7 @@ class GameSeeder extends Seeder
     public function run()
     {
         User::factory()
-            ->hasGames(24)
+            ->hasGames(92)
             ->create();
     }
 }
