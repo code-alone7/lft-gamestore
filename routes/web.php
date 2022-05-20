4960670e43a838a94c398b7f754f38cc6cfa8763
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-20 05:34:41 +0500

    remove unused route

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
