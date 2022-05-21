19962a2163e9ea3e6d72c9ee9f261f81f4b4521a
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-21 15:37:47 +0500

    refactor GameFactory

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -18,18 +18,10 @@ class GameFactory extends Factory
      */
     public function definition()
     {
-        $desc = '';
-
-        for ($i = 0; $i < rand(2, 5); $i++) {
-            $desc .= '<p>';
-            $desc .= $this->faker->realText(rand(200, 600));
-            $desc .= '</p>';
-        }
-
         return [
             'title' => $this->faker->unique()->text(rand(10,30)),
             'price' => rand(1, 30) * 100,
-            'description' => $desc,
+            'description' => $this->formattedText(rand(2, 5), rand(200, 600)),
             'photo' => 'https://picsum.photos/250/145',
         ];
     }
@@ -40,13 +32,34 @@ class GameFactory extends Factory
             $gCount = Genre::query()->count();
 
             if ($gCount) {
+                // Attach one to three random genres to the game.
                 $genresID = Genre::query()
                     ->inRandomOrder()
-                    ->take(rand(1, min(3, $gCount)))
+                    ->take(rand(1, min(3, $gCount))) // min() in case genres count < 3.
                     ->pluck('id');
 
                 $game->genres()->attach($genresID);
             }
         });
+    }
+
+    /**
+     * Randomly generate paragraphs wrapped in <p> tag.
+     *
+     * @param int $pNum amount of paragraphs.
+     * @param int $pLength length of paragraphs.
+     * @return string
+     */
+    private function formattedText(int $pNum, int $pLength): string
+    {
+        $text = '';
+
+        for ($i = 0; $i < $pNum; $i++) {
+            $text .= '<p>';
+            $text .= $this->faker->realText($pLength);
+            $text .= '</p>';
+        }
+
+        return $text;
     }
 }
