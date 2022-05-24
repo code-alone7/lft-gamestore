68a49ec4d5e59065c73bd946fb9d154ef04e791d
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-05-24 18:26:08 +0500

    create ordser_statuses seeder

---
+++ database/seeders/OrderStatusSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\OrderStatus;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrderStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {        
+        OrderStatus::create(['title' => 'unpaid']);
+        OrderStatus::create(['title' => 'paid']);
+    }
+}
